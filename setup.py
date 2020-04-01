2f5d65f9ec613096d2e1b294215ffd4b20a99efd
silkenpy <silkenpy@gmail.com>
2020-04-01 16:11:45 +0430

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="arvan",  # Replace with your own username
+    version="0.0.1",
+    author="silkenpy",
+    author_email="silkenpy@gmail.com",
+    description="Arvan is python api to work with arvancloud",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/silkenpy/arvan",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
